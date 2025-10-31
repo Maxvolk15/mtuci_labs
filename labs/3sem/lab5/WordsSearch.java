fdec5a5eb2ef5ecbbaa1c615834587f7da4c165d
Maxvolk <maxyma01@gmail.com>
2025-10-31 07:47:35 +0300

    Added lab5 final vers (docx)

--- labs/3sem/lab5/WordsSearch.java
+++ labs/3sem/lab5/WordsSearch.java
@@ -18,15 +18,21 @@ public class WordsSearch {
     }
 
     public static void wordsFinder(String text, String letter) {
+        if (letter.length() == 0) {
+            System.out.println("Буква не указана");
+            return;
+        }
         Pattern patt = Pattern.compile("\\b[" + letter.toLowerCase() + letter.toUpperCase() + "][a-zA-Z]*\\b");
         Matcher match = patt.matcher(text);
-        if (!match.find()) {
+        System.out.println("Слова в тексте, начинающиеся на букву " + letter + ": ");
+        boolean flag = true;
+        while (match.find()) {
+            System.out.println(match.group());
+            flag = false;
+        }
+
+        if (flag) {
             System.out.println("Слова в тексте, начинающиеся на букву " + letter + " не найдены");
-        } else {
-            System.out.println("Слова в тексте, начинающиеся на букву " + letter + ": ");
-            while (match.find()) {
-                System.out.println(match.group());
-            }
         }
     }
 }
