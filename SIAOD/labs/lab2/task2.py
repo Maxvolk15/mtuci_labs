0d664bda378ff7eefdfe73109560ed58b70c2a0c
Maxvolk15 <maxyma01@gmail.com>
2026-03-16 19:55:18 +0300

    Added siaod and lab2 beta

---
+++ SIAOD/labs/lab2/task2.py
@@ -0,0 +1,23 @@
+import time
+
+def cycle(n):
+    if n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n+1):
+            result *= i
+        return result
+
+def recursive(n):
+    if n == 0:
+        return 1
+    else:
+        return n * recursive(n-1)
+
+start_time = time.time()
+print(f"Cycle: {cycle(1000)}")
+print("Cycle time: %s seconds" % (time.time() - start_time))
+start_time = time.time()
+print(f"Recursive: {recursive(1000)}")
+print("Recursive time: %s seconds" % (time.time() - start_time))
